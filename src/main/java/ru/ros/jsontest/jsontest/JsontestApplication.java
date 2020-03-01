3a30df165c20faf49b082e3c31850d8d0f655923
KAE-dev <kenobidark@gmail.com>
2020-03-01 23:37:46 +0300

    init commit

---
+++ src/main/java/ru/ros/jsontest/jsontest/JsontestApplication.java
@@ -0,0 +1,15 @@
+package ru.ros.jsontest.jsontest;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.jsonschema.JsonSchema;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsontestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ros/jsontest/jsontest/JsontestApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.ros.jsontest.jsontest;
+
+import org.junit.jupiter.api.Test;
+
+
+class JsontestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
